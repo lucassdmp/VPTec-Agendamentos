d8124a1668f45bd46d713cd763bcaade74e214cc
Lucas <lucassdmp01@gmail.com>
2024-10-02 01:20:01 -0300

    fix: moved all pages to core for better organization later on

--- Core/Classes/Admin/RegisterService.php
+++ Core/Classes/Admin/RegisterService.php
@@ -4,7 +4,7 @@ namespace VPTec\Agendamentos\Core\Ajax\Service;
 
 use VPTec\Agendamentos\Templates\RegisterServiceForm;
 
-require_once plugin_dir_path( __FILE__ ) . "/../../../Templates/RegisterServiceForm.php";
+require_once plugin_dir_path( __FILE__ ) . "/../../Templates/RegisterServiceForm.php";
 
 class RegisterService {
     public function __construct(){
